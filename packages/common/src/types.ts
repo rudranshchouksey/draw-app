1938281c2e0257a25e17a3c049a226d21a0e4974
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2025-02-06 18:09:55 +0530

    first commit

---
+++ packages/common/src/types.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+export const CreateUserSchema = z.object({
+    username: z.string().min(3).max(20),
+    password: z.string().min(6).max(100),
+    name: z.string().min(3).max(100),
+})
+
+export const SigninSchema = z.object({
+    username: z.string().min(3).max(20),
+    password: z.string().min(6).max(100),
+})
+
+export const CreateRoomSchema = z.object({
+    name: z.string().min(3).max(100),
+})
